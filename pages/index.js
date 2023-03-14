b7f3829637af85b7066f42a0109308648e49bdf1
Lucas Marcelo <65972119+lucasfemar@users.noreply.github.com>
2023-03-14 15:50:00 +0000

    Atualizando meu index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Teste1</h1>;
+  return <h1>Amor, fiz mais esse deply pra dizer a você o quão maravilhosa vocês é. Te amo tanto meu amor!</h1>;
 }
 
 export default Home;
