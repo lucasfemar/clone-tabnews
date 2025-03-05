ce6522560bbbcd3fc69f8ac707da9d4e8627d662
Lucas Marcelo <65972119+lucasfemar@users.noreply.github.com>
2025-03-05 21:05:07 +0000

    refactor: use `next-connector` in `/migrations` and `/status`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,56 +2,50 @@ import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database";
 import { createRouter } from "next-connect";
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
-
+import controler from "infra/controller";
 const router = createRouter();
-router.get(getHandler).post(postHandler);
+router.get(getHandler);
+router.post(postHandler);
 
-export default router.handler({
-  onNoMatch: onNoMachHandler,
-  onError: onErrorHandler,
-});
+export default router.handler(controler.errorHandlers);
 
-function getDefaultMigrationOptions(dbClient) {
-  return {
-    dbClient,
-    dryRun: true,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-}
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
-
-function onNoMachHandler(request, response) {
-  const publicErrorObject = new MethodNotAllowedError();
-  return response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
 
 async function postHandler(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = getDefaultMigrationOptions(dbClient);
-  const migratedMigrations = await migrationRunner({
-    ...defaultMigrationOptions,
-    dryRun: false, // Rondando no liveRun
-  });
-  await dbClient.end();
-  if (migratedMigrations.length > 0) {
-    return response.status(201).json(migratedMigrations);
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient: dbClient,
+      dryRun: false, // Rondando no liveRun
+    });
+    await dbClient.end();
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  } finally {
+    dbClient.end();
   }
-  return response.status(200).json(migratedMigrations);
 }
 
 async function getHandler(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = getDefaultMigrationOptions(dbClient);
-  const pedingMigrations = await migrationRunner(defaultMigrationOptions);
-  await dbClient.end();
-  return response.status(200).json(pedingMigrations);
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const pedingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+    return response.status(200).json(pedingMigrations);
+  } finally {
+    dbClient.end();
+  }
 }
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,29 +1,11 @@
-import { createRouter } from "next-connect";
+import controler from "infra/controller";
 import database from "infra/database.js";
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
-
+import { createRouter } from "next-connect";
 const router = createRouter();
 
 router.get(getHandler);
 
-export default router.handler({
-  onNoMatch: onNoMatchHandler,
-  onError: onErrorHandler,
-});
-
-function onNoMatchHandler(request, response) {
-  const publicErrorObject = new MethodNotAllowedError();
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
-
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-  console.log("\n Erro dentro do catch do next-connect:");
-  console.error(publicErrorObject);
-  response.status(500).json(publicErrorObject);
-}
+export default router.handler(controler.errorHandlers);
 
 async function getHandler(request, response) {
   const updatedAt = new Date().toISOString(); //Retorna a data no padrão ISO8601
