e537e5d345bf1174cca06a36a22dd4c5e38ee3eb
Lucas Marcelo <65972119+lucasfemar@users.noreply.github.com>
2025-03-07 00:04:22 +0000

    refactor: make `/api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,51 +1,21 @@
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database";
 import { createRouter } from "next-connect";
 import controler from "infra/controller";
+import migrator from "models/migrator.js";
 const router = createRouter();
 router.get(getHandler);
 router.post(postHandler);
 
 export default router.handler(controler.errorHandlers);
 
-const defaultMigrationOptions = {
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
-
-async function postHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient: dbClient,
-      dryRun: false, // Rondando no liveRun
-    });
-    await dbClient.end();
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-    return response.status(200).json(migratedMigrations);
-  } finally {
-    dbClient.end();
-  }
+async function getHandler(request, response) {
+  const pedingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pedingMigrations);
 }
 
-async function getHandler(request, response) {
-  let dbClient;
-  try {
-    dbClient = await database.getNewClient();
-    const pedingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dbClient,
-    });
-    return response.status(200).json(pedingMigrations);
-  } finally {
-    dbClient.end();
+async function postHandler(request, response) {
+  const migratedMigrations = await migrator.runPendingMigrations();
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
+  return response.status(200).json(migratedMigrations);
 }
