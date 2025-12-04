19cceb469814ccfe5d510d2a05e7502916541cd3
Lucas Marcelo <lucasdeveloper98l@gmail.com>
2025-12-04 20:50:28 -0300

    feat: implement `api/v1/user` endpoint with session renewal

--- infra/controller.js
+++ infra/controller.js
@@ -1,3 +1,5 @@
+import * as cookie from "cookie";
+import session from "models/session.js";
 import {
   InternalServerError,
   MethodNotAllowedError,
@@ -26,11 +28,27 @@ function onNoMachHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
   return response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
+
+function setSessionCookie(sessionToken, response) {
+  const setCookie = cookie.serialize("session_id", sessionToken, {
+    path: "/",
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+  response.setHeader("Set-Cookie", setCookie);
+  /* 
+    Path=/ -> Faz com que o cookie seja colocado no path raiz do site 
+    fazendo com ele fique disponível em todas as rotas. 
+  */
+}
+
 const controller = {
   errorHandlers: {
     onNoMatch: onNoMachHandler,
     onError: onErrorHandler,
   },
+  setSessionCookie,
 };
 
 export default controller;
--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -1,4 +1,3 @@
-import * as cookie from "cookie";
 import controler from "infra/controller";
 import authentication from "models/authentication.js";
 import session from "models/session.js";
@@ -18,17 +17,8 @@ async function postHandler(request, response) {
   );
 
   const newSession = await session.create(authenticatedUser.id);
-  const setCookie = cookie.serialize("session_id", newSession.token, {
-    path: "/",
-    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
-    secure: process.env.NODE_ENV === "production",
-    httpOnly: true,
-  });
-  response.setHeader("Set-Cookie", setCookie);
-  /* 
-    Path=/ -> Faz com que o cookie seja colocado no path raiz do site 
-    fazendo com ele fique disponível em todas as rotas. 
-  */
+
+  controler.setSessionCookie(newSession.token, response);
 
   return response.status(201).json(newSession);
 }
