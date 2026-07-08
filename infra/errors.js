29c8ebb7c87fb76da0f94c091dc1074e9b3f359f
Lucas Marcelo <lucasdeveloper98l@gmail.com>
2026-07-07 22:16:55 -0300

    refactor: improve error loggin in `email.send()`

--- infra/errors.js
+++ infra/errors.js
@@ -19,13 +19,14 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indiponível no momento.", {
       cause,
     });
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço esta diponível.";
+    this.action = action || "Verifique se o serviço esta diponível.";
     this.statusCode = 503;
+    this.context = context;
   }
 
   toJSON() {
@@ -34,6 +35,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context,
     };
   }
 }
